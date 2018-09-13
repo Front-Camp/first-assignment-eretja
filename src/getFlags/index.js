ab77f1ba8c08fe894f42f251abfe0b9dd6f96a0e
Khrystyna Koshulynska <eretja@gmail.com>
2018-09-13 19:44:08 +0200

    Update index.js

--- src/getFlags/index.js
+++ src/getFlags/index.js
@@ -2,11 +2,15 @@
  * @param {Array<Object>} arr - this is an array of objects
  * @return {Array} result of the sum value.
  * @example
- * const arr = [{flags: [1, 2, 3]}, {flags: [4, 5, 6]}];
+  const arr = [{flags: [1, 2, 3]}, {flags: [4, 5, 6]}];
  * getFlags(arr); // [1, 2, 3, 4, 5, 6]
  */
 const getFlags = arr => {
-  /* your logic here...*/
+    var result = arr[0].flags;
+    for (i = 0; i < arr[1].flags.length; ++i) {
+        result[result.length] = arr[1].flags[i];
+    }
+    return result;
 };
 
 export default getFlags;
