5dbf04513294259d3170e332c017ef9eb1927beb
Khrystyna Koshulynska <eretja@gmail.com>
2018-09-13 19:48:18 +0200

    Update index.js

--- src/truncate/index.js
+++ src/truncate/index.js
@@ -8,7 +8,11 @@
  * @return {string} truncated string
  */
 const truncate = (str, length = 0, replacer = '...') => {
-  /* your logic here...*/
-};
+        export const truncate = (str, length = 0, ending = '...') => {
+            if (str.length > length) {
+                return str.substring(0, length - ending.length) + ending;
+            }
+            return str;
+        };
 
-export default truncate;
+        export default truncate;
